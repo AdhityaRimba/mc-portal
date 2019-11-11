e6bb698217bfbc0bbe6cdbd653d87f3198ca39cd
AdhityaRimba <47937950+AdhityaRimba@users.noreply.github.com>
2019-11-11 23:39:39 +0000

    mc-portal

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php 
+// Title
+$title = ("Server Portal");
+// Your Server IP
+$ip = ("mc.hypixel.net");
+// Page URL
+$forum = ("#"); // Change To Your Forum URL
+$vote =("#"); // Change To Your Vote URL
+$guide = ("#"); // Change To Your Guide URL
+
+// Counter Color (Light / Dark)
+$color = ("dark");
+ ?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php 
+require "config.php";
+require "function.php";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?= $title; ?></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+	<div class="row d-flex justify-content-center">
+		<div class="col-md-auto mt-3">
+			<div class="logo"></div>
+		</div>
+	</div>
+	<div class="row d-flex justify-content-center">
+		<div class="col-md-auto">
+			<div class="counter text-center shadow bg-<?= $color; ?> p-2 font-weight-bold text-uppercase mt-3"><?= $ip; ?> |  <?php 
+			if( $online == true ) {
+			 ?>Online <?= $player; ?>
+			 <?php 
+			}else{
+			  ?>
+			  <p class="text-danger">Offline</p>
+			<?php } ?>
+			 </div>
+		</div>
+	</div>
+	<br><br>
+	<div class="row d-flex justify-content-center">
+		<div class="col-md-3">
+			<a href="<?= $forum; ?>"><div class="forum"></div></a>
+		</div>
+		<div class="col-md-3">
+			<a href="<?= $vote; ?>"><div class="vote"></div></a>
+		</div>
+		<div class="col-md-3">
+			<a href="<?= $guide; ?>"><div class="guide"></div></a>
+		</div>
+	</div>
+</div>
+</body>
+</html>
