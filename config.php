eb9e91104a3c37adff9884827f11d9561fadf081
AdhityaRimba <47937950+AdhityaRimba@users.noreply.github.com>
2019-11-12 11:53:45 +0000

    Added Image Config

--- config.php
+++ config.php
@@ -5,8 +5,10 @@ $title = ("Server Portal");
 $ip = ("mc.hypixel.net");
 // Page URL
 $forum = ("#"); // Change To Your Forum URL
-$vote =("#"); // Change To Your Vote URL
+$vote = ("#"); // Change To Your Vote URL
 $guide = ("#"); // Change To Your Guide URL
-
+// Page Image
+$img_forum = ("img/forum.png"); // Forum Image
+$img_vote = ("img/vote.png"); // Vote Image
+$img_guide = ("img/guide.png"); // Guide Image
  ?>
- 
